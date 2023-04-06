4574b3cbda3ad88308586d24a19b1c3bc5013f4b
titanium-cmd <nkansahisaac41@gmail.com>
2023-04-06 00:33:33 +0000

    mongodb setup done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/indexRouter');
 const usersRouter = require('./routes/usersRouter');
@@ -18,4 +19,10 @@ app.use('/users', usersRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${process.env.PORT}`);
+  console.log(`CONNECTING TO MONGO DB...`);
+  mongoose.connect(process.env.MONGODB_ATLAS).then(() => {
+    console.log('MONGODB CONNECTED');
+  }).catch((error) => {
+    console.log('MONGODB CONNECTION ERROR:: ', error.message);
+  });
 })
