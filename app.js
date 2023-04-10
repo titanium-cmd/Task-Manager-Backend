ad173de647745ae78f9f1f64601f19c0065dd2bb
titanium-cmd <nkansahisaac41@gmail.com>
2023-04-10 02:03:48 +0000

    bug fixes

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/tasks', tasksRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`SERVER RUNNING ON PORT ${process.env.PORT}`);
-  console.log(`CONNECTING TO MONGO DB...`);
-  mongoose.connect(process.env.MONGODB_ATLAS).then(() => {
-    console.log('MONGODB CONNECTED');
-  }).catch((error) => {
-    console.log('MONGODB CONNECTION ERROR:: ', error.message);
-  });
-})
+mongoose.connect(process.env.MONGODB_ATLAS).then(() => {
+  console.log('MONGODB CONNECTED');
+  app.listen(process.env.PORT, () => {
+    console.log(`SERVER RUNNING ON PORT ${process.env.PORT}`);
+    console.log(`CONNECTING TO MONGO DB...`);
+  })
+}).catch((error) => {
+  console.log('MONGODB CONNECTION ERROR:: ', error.message);
+});
